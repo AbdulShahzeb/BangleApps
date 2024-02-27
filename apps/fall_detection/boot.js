a1681a037eaa3e1968e699fcd0f0e3d98b733c6a
Abdul Shahzeb <z5311131@ad.unsw.edu.au>
2024-02-27 14:37:42 +1100

    testing all 3 data advertisement

--- apps/fall_detection/boot.js
+++ apps/fall_detection/boot.js
@@ -1,8 +1,8 @@
 (() => {
   const HR_SERVICE = 0x180D;
   const HR_UUID = 0x2A37;
-  //const MOTION_SERVICE = "00030000-78fc-48fe-8e23-433b3a1942d0";
-  //const MOTION_UUID = "00030002-78fc-48fe-8e23-433b3a1942d0";
+  const MOTION_SERVICE = 0xFFA0;
+  const MOTION_UUID = 0xFFA3;
   const GPS_SERVICE = 0x1819;
   const LAT_UUID = 0x2AAE;
   const LON_UUID = 0x2AAF;
@@ -10,13 +10,14 @@
 
   function setupAdvertising() {
     /*
-     * This function prepares BLE heart rate Advertisement.
+     * This function prepares BLE advertisement.
      */
 
     NRF.setAdvertising(
       {
         0x180D: undefined,
         0x1819: undefined,
+        0xFFA0: undefined,
       },
       {
         // Set up advertisement settings
@@ -37,18 +38,24 @@
           value: 0x02,
         }
       },
-      0x1819: {
+      0x1819: { // gps
         0x2AAE: {
           notify: true,
-          value: 0,
+          value: [0,0,0,0], // lat
         },
         0x2AAF: {
           notify: true,
-          value: 0,
+          value: [0,0,0,0], // lon
         },
         0x2A2B: {
           notify: true,
+          value: [0,0,0,0,0,0,0,0,0,0,0,0,0], // time
+        }
+      },
+      0xFFA0: {
+        0xFFA3: {
           value: 0,
+          notify: true,
         }
       }
     });
@@ -84,29 +91,6 @@
     }
   }
 
-  /*function updateMotion(xyz) {
-    if (xyz === undefined) return;
-
-    var abs_acc = Math.sqrt(accel.x*accel.x + accel.y*accel.y + accel.z*accel.z) - 100;
-    if (abs_acc < 0) { abs_acc = 0; }
-
-    try {
-      NRF.updateServices({
-        MOTION_SERVICE: {
-          MOTION_UUID: {
-            value: abs_acc,
-            notify: true
-          }
-        }
-      });
-    } catch (error) {
-      if (error.message.includes("BLE restart")) {
-        NRF.disconnect();
-      } else {
-        console.log("[fall_detection]: Unexpected error occured while updating Motion over BLE! Error: " + error.message);
-      }
-    }
-  }*/
 
   function updateGPS(fix) {
     /*
@@ -114,23 +98,41 @@
      */
     if (fix === undefined || fix.fix === 0) return;
 
+    var latBuffer = new ArrayBuffer(4);
+    var latView = new Uint32Array(latBuffer);
+    latView[0] = Math.floor(fix.lat * 10000);
+
+    var lonBuffer = new ArrayBuffer(4);
+    var lonView = new Uint32Array(lonBuffer);
+    lonView[0] = Math.floor(fix.lon * 10000);
+
+    var timeBuffer = new ArrayBuffer(8);
+    var timeView = new DataView(timeBuffer);
+    var currentTime = Date.now().toString();
+    var dateArray = [];
+
+    for (var i = 0; i < 13; i++) {
+        dateArray.push(parseInt(currentTime.charAt(i)));
+    }
+
     try {
       NRF.updateServices({
         0x1819: {
           0x2AAE: {
-            value: fix.lat,
+            value: latBuffer,
             notify: true,
           },
           0x2AAF: {
-            value: fix.lon,
+            value: lonBuffer,
             notify: true,
           },
           0x2A2B: {
-            value: fix.time,
+            value: dateArray,
             notify: true,
-          }
+          },
         }
       });
+
     } catch (error) {
       if (error.message.includes("BLE restart")) {
         /*
@@ -143,11 +145,41 @@
     }
   }
 
+  function updateMotion(xyz) {
+    if (xyz === undefined) return;
+
+    var d2 = [
+      Math.round(xyz.x*100) * Math.round(xyz.x*100),
+      Math.round(xyz.y*100) * Math.round(xyz.y*100),
+      Math.round(xyz.z*100) * Math.round(xyz.z*100)
+    ];
+    var abs_acc = Math.sqrt(d2[0] + d2[1] + d2[2]) - 100;
+    if (abs_acc < 0) { abs_acc = 0; }
+
+    try {
+      NRF.updateServices({
+        0xFFA0: {
+          0xFFA3: {
+            value: abs_acc,
+            notify: true
+          }
+        }
+      });
+    } catch (error) {
+      if (error.message.includes("BLE restart")) {
+        NRF.disconnect();
+      } else {
+        console.log("[fall_detection]: Unexpected error occured while updating Motion over BLE! Error: " + error.message);
+      }
+    }
+  }
+
+
   setupAdvertising();
   Bangle.setHRMPower(1);
   Bangle.setGPSPower(1);
   Bangle.on("HRM", function (hrm) { updateHRM(hrm); });
-  //Bangle.on("accel", function(xyz) { updateMotion(xyz); });
+  Bangle.on("accel", function (xyz) { updateMotion(xyz); });
   Bangle.on("GPS", function(fix) { updateGPS(fix); });
 
 })();
